e80f4ecc49eeafd1fa17eef290c87f3675f3126d
MyEcoria <contact@myecoria.com>
2024-04-14 11:00:18 +0200

    push

--- lib/root_checker.dart
+++ lib/root_checker.dart
@@ -5,7 +5,7 @@ import 'package:flutter/services.dart';
 class RootChecker {
   static const MethodChannel _channel = const MethodChannel('root_checker');
 
-  static Future<bool> get isDeviceRooted async{
+  static Future<bool?> get isDeviceRooted async{
     return await _channel.invokeMethod('isDeviceRooted');
   }
 }
